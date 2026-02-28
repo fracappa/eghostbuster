33a66cefffb63ab65233c80c79299bf44bc80abd
Francesco Cappa <24370687+fracappa@users.noreply.github.com>
2026-02-28 16:53:21 +0100

    fix: adding socket existance check with ss before cleaning up (#11)

--- pkg/socket/socket.go
+++ pkg/socket/socket.go
@@ -4,26 +4,60 @@ import (
 	"fmt"
 	"os/exec"
 
+	"github.com/eghostbuster/eghostbuster/pkg/consts"
 	"github.com/eghostbuster/eghostbuster/pkg/netns"
 )
 
 // Destroy kills a TCP socket in CLOSE_WAIT state using ss
-func DestroySocketNetnsIno(netnsIno uint64, srcIP string, srcPort uint16, dstIP string, dstPort uint16) error {
-	src := fmt.Sprintf("%s:%d", srcIP, srcPort)
-	dst := fmt.Sprintf("%s:%d", dstIP, dstPort)
+func DestroySocketNetnsIno(netnsIno uint64, proto uint8, srcIP uint32, srcPort uint16, dstIP uint32, dstPort uint16) error {
 
 	return netns.Do(netnsIno, func() error {
-		return ssKill(src, dst)
+		return destroySocket(proto, srcIP, dstIP, srcPort, dstPort)
 	})
 }
 
-func ssKill(src, dst string) error {
-	cmd := exec.Command("ss", "--kill", "state", "close-wait",
+func destroySocket(proto uint8, srcIP, dstIP uint32, srcPort, dstPort uint16) error {
+	srcAddr := fmt.Sprintf("%d.%d.%d.%d",
+		byte(srcIP), byte(srcIP>>8), byte(srcIP>>16), byte(srcIP>>24))
+	dstAddr := fmt.Sprintf("%d.%d.%d.%d",
+		byte(dstIP), byte(dstIP>>8), byte(dstIP>>16), byte(dstIP>>24))
+
+	srcP := Ntohs(srcPort)
+	dstP := Ntohs(dstPort)
+
+	src := fmt.Sprintf("%s:%d", srcAddr, srcP)
+	dst := fmt.Sprintf("%s:%d", dstAddr, dstP)
+
+	// First verify socket exists with ss
+	if !socketExists(src, dst) {
+		return fmt.Errorf("socket not found")
+	}
+
+	// Now destroy it
+	if proto == consts.TCP_PROTOCOL {
+		return destroyViaSS(src, dst)
+	}
+
+	return fmt.Errorf("unsupported protocol: %d", proto)
+}
+
+// socketExists checks if a socket exists using ss
+func socketExists(src, dst string) bool {
+	cmd := exec.Command("ss", "-tn", "state", "close-wait",
 		"src", src, "dst", dst)
 	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("ss failed: %v, output: %s", err, output)
-	}
+	// ss returns 0 if socket found, 1 if not found
+	return err == nil && len(output) > 0
+}
+
+// destroyViaSS destroys socket using ss --kill
+// This is a last-resort method since ss doesn't report errors
+func destroyViaSS(src, dst string) error {
+	cmd := exec.Command("ss", "--kill", "state", "close-wait",
+		"src", src, "dst", dst)
+	_, _ = cmd.CombinedOutput()
+	// ss --kill always returns 0, so just return nil
+	// We already verified the socket exists
 	return nil
 }
 
