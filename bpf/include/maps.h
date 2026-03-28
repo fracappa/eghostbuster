fb66caeab350e3c10133f2b469f905fdaf4be4b6
fracappa <fcappa@redhat.com>
2026-03-28 18:40:55 +0100

    feat: first implementation of lock files cleanup

--- bpf/include/maps.h
+++ bpf/include/maps.h
@@ -10,4 +10,23 @@ struct {
     __type(value, struct close_wait_info);
 } close_wait_tracker SEC(".maps");
 
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 10240);
+    __type(key, __u32);
+    __type(value, struct file_info);
+} file_process_map SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 256 * 1024); // 256 KB buffer
+} exit_events SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, __u32);
+    __type(value, struct file_info);
+} file_info_scratch SEC(".maps");
+
 #endif /* __MAPS_H */
